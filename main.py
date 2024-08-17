27e0abc0f207a11e8c1d7b3d2c2fe8877c6f1885
khurramaleee <khurramali63@gmail.com>
2024-08-17 14:43:18 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import sortingalgorithms as sortingclass
+
+arr = [2,3,3,4544,5,63,2,98]
+sorting = sortingclass.sorting()
+print(sorting.bubbleSort(arr))
---
+++ sortingalgorithms.py
@@ -0,0 +1,16 @@
+class sorting:
+    def __init__(self):
+        pass
+    def bubbleSort(self,arr):
+        for i in range(len(arr)):
+            for j in range(1, len(arr)):
+                if arr[j] < arr[j - 1]:
+                    self.swap(arr,j,j-1)
+        return arr
+    def selectionSort(self,arr):
+        pass
+
+    def swap(self, arr, a, b):
+        temp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = temp
